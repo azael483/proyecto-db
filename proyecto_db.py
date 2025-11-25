23e045ff5a7d7b78eb811e7409a5dff8fc62883a
azael483 <azaelortega483@gmail.com>
2025-11-24 20:49:26 -0500

    cambio 1
    Añadida integración Flask y conexión MySQL
    
    Se ha integrado Flask para crear una aplicación web con una nueva ruta `/proyecto/<int:id>` que utiliza un procedimiento almacenado en MySQL para obtener datos de un proyecto.
    
    - Importadas las librerías `mysql.connector`, `tabulate` y `flask`.
    - Configurada la conexión a la base de datos MySQL.
    - Añadida la función `proyecto(id)` para manejar la lógica de la ruta.
    - Renderización de datos en la plantilla `proyecto.html`.
    - Configurado el servidor Flask para ejecutarse en modo de depuración.

--- proyecto_db.py
+++ proyecto_db.py
@@ -1,1 +1,33 @@
+import mysql.connector
+from tabulate import tabulate
+from flask import Flask, render_template
 
+conexion = mysql.connector.connect(
+    host="localhost",
+    user="root",
+    password="Azael483",
+    database="constructoradb"
+)
+
+app = Flask(__name__)
+
+@app.route("/proyecto/<int:id>")
+#@app.route("/proyecto/la_constructora_de_Bob")
+def proyecto(id):
+    conexion = conectar()
+    cursor = conexion.cursor()
+    cursor.callproc("PresupuestoProyecto", [id])
+
+    for result in cursor.stored_results():
+        filas = result.fetchall()
+        columnas = [desc[0] for desc in result.description]
+
+    cursor.close()
+    conexion.close()
+
+    return render_template("proyecto.html", columnas=columnas, filas=filas)
+
+app.run(debug=True)
+
+if __name__ == "__main__":
+    app.run(debug=True)
